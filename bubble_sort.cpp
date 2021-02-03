f7c23f9529a1dacd212b034252848804fcb6914f
Utkarsh Singh <utkarsh190601@gmail.com>
2021-02-03 21:10:30 +0530

    sorting: fix bubble sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,19 +1,21 @@
+// implement bubble sort
+
 #include<iostream>
 #include<vector>
 #include<utility>
 
 template <typename T>
-
-void bubble_sort(std :: vector<T> &v, bool cmp(T, T)=[](T a, T b){return a < b;}){
-    for(size_t i = 0; i < v.size()-1; i++){
-        bool swapped = false;
+void bubble_sort(std::vector<T> &v,
+		 bool cmp(T, T)=[](T a, T b){return a < b;})
+{
+    bool swapped = false;
+    for(size_t i = 0; i < v.size() && !swapped; i++){
+	swapped = true;
         for(size_t j = 0; j < v.size()-i-1; j++){
-            if(cmp(v[j], v[j+1])){
-                std :: swap(v[j], v[j+1]);
-                swapped = true;
+            if(cmp(v[j+1], v[j])) {
+                std::swap(v[j], v[j+1]);
+                swapped = false;
             } 
-            if(!swapped) break;
         }
     }
-    return;
 }
