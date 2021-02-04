396b4ef1a5b25c7156b8a1511df3be35ac9e5196
Utkarsh Singh <utkarsh190601@gmail.com>
2021-02-04 15:50:45 +0530

    change indent style from gnu to stroustrup

--- selection_sort.h
+++ selection_sort.h
@@ -1,4 +1,4 @@
-// implement selection sort for different containers
+// implement selection sort
 
 #include <iostream>
 #include <vector>
@@ -8,17 +8,17 @@ template<typename T>
 void selection_sort(std::vector<T>& vec,
 		    bool cmp(T, T)=[](T a, T b){return a<b;})
 {
-  // selected index for sort
-  size_t sel;
+    // selected index for sort
+    size_t sel;
   
-  for(size_t i=0; i<vec.size(); i++) {
-    sel = i;
+    for(size_t i=0; i<vec.size(); i++) {
+	sel = i;
 
-    // search element with cmp
-    for(size_t j = i+1; j<vec.size(); j++)
-      if (cmp(vec[j], vec[sel])) sel = j;
+	// search element with cmp
+	for(size_t j = i+1; j<vec.size(); j++)
+	    if (cmp(vec[j], vec[sel])) sel = j;
 
-    // bring selected element to top
-    std::swap(vec[i], vec[sel]);
-  }
+	// bring selected element to top
+	std::swap(vec[i], vec[sel]);
+    }
 }
--- test.cpp
+++ test.cpp
@@ -1,5 +1,6 @@
 #include "selection_sort.h"
 #include "insertion_sort.h"
+#include "heap_sort.h"
 #include <exception>
 
 using namespace std;
@@ -8,8 +9,8 @@ using namespace std;
 template<typename T>
 ostream& operator<<(ostream& os, const vector<T>& vec)
 {
-  for(auto i:vec) os << i << " ";
-  return os;
+    for(auto i:vec) os << i << " ";
+    return os;
 }
 
 // compare function, used for descending order sort
@@ -18,101 +19,145 @@ bool is_more(int a, int b) { return a>b;}
 // test selection sort
 void test1()
 {
-  vector<int> v0{0, 22, 3, 2, 21};
-  selection_sort(v0);
-  cout << "v0 = " << v0 << "\n";
+    vector<int> v0{0, 22, 3, 2, 21};
+    selection_sort(v0);
+    cout << "v0 = " << v0 << "\n";
   
-  // test large input
-  vector<int> v1;
-  for(int i=1000; i>0; i--) v1.push_back(i);
-  selection_sort(v1);
-  // cout << "v1 = " << v1 << "\n";
+    // test large input
+    vector<int> v1;
+    for(int i=1000; i>0; i--) v1.push_back(i);
+    selection_sort(v1);
+    // cout << "v1 = " << v1 << "\n";
 
-  // test 0 as input
-  vector<int> v2;
-  selection_sort(v2);
-  cout << "v2 = " << v2 << "\n";
+    // test 0 as input
+    vector<int> v2;
+    selection_sort(v2);
+    cout << "v2 = " << v2 << "\n";
 
-  // test negative numbers
-  vector<int> v3;
-  for(int i=-1; i>-11; i--) v3.push_back(i);
-  selection_sort(v3);
-  cout << "v3 = " << v3 << "\n";
+    // test negative numbers
+    vector<int> v3;
+    for(int i=-1; i>-11; i--) v3.push_back(i);
+    selection_sort(v3);
+    cout << "v3 = " << v3 << "\n";
 
-  // test 1's as input
-  vector<int> v4{1, 1, 1, 1, 1};
-  selection_sort(v4);
-  cout << "v4 = " << v4 << "\n";
+    // test 1's as input
+    vector<int> v4{1, 1, 1, 1, 1};
+    selection_sort(v4);
+    cout << "v4 = " << v4 << "\n";
 
-  // test 0's as input
-  vector<int> v5(10);
-  // selection_sort(v5);
-  cout << "v5 = " << v5 << "\n";
+    // test 0's as input
+    vector<int> v5(10);
+    // selection_sort(v5);
+    cout << "v5 = " << v5 << "\n";
 
-  // test 0's and 1's
-  vector<int> v6;
-  for(int i=0; i<10; i++) {
-    if(i%2 == 0) v6.push_back(1);
-    else v6.push_back(0);
-  }
-  selection_sort(v6, is_more);
-  cout << "v6 = " << v6 << "\n";
+    // test 0's and 1's
+    vector<int> v6;
+    for(int i=0; i<10; i++) {
+	if(i%2 == 0) v6.push_back(1);
+	else v6.push_back(0);
+    }
+    selection_sort(v6, is_more);
+    cout << "v6 = " << v6 << "\n";
 }
 
 // test insertion sort
 void test2()
 {
-  vector<int> v0{0, 22, 3, 2, 21};
-  insertion_sort(v0);
-  cout << "v0 = " << v0 << "\n";
+    vector<int> v0{0, 22, 3, 2, 21};
+    insertion_sort(v0);
+    cout << "v0 = " << v0 << "\n";
   
-  // test large input
-  vector<int> v1;
-  for(int i=1000; i>0; i--) v1.push_back(i);
-  insertion_sort(v1);
-  // cout << "v1 = " << v1 << "\n";
+    // test large input
+    vector<int> v1;
+    for(int i=1000; i>0; i--) v1.push_back(i);
+    insertion_sort(v1);
+    // cout << "v1 = " << v1 << "\n";
 
-  // test 0 as input
-  vector<int> v2;
-  insertion_sort(v2);
-  cout << "v2 = " << v2 << "\n";
+    // test 0 as input
+    vector<int> v2;
+    insertion_sort(v2);
+    cout << "v2 = " << v2 << "\n";
 
-  // test negative numbers
-  vector<int> v3;
-  for(int i=-1; i>-11; i--) v3.push_back(i);
-  insertion_sort(v3);
-  cout << "v3 = " << v3 << "\n";
+    // test negative numbers
+    vector<int> v3;
+    for(int i=-1; i>-11; i--) v3.push_back(i);
+    insertion_sort(v3);
+    cout << "v3 = " << v3 << "\n";
 
-  // test 1's as input
-  vector<int> v4{1, 1, 1, 1, 1};
-  insertion_sort(v4);
-  cout << "v4 = " << v4 << "\n";
+    // test 1's as input
+    vector<int> v4{1, 1, 1, 1, 1};
+    insertion_sort(v4);
+    cout << "v4 = " << v4 << "\n";
 
-  // test 0's as input
-  vector<int> v5(10);
-  // insertion_sort(v5);
-  cout << "v5 = " << v5 << "\n";
+    // test 0's as input
+    vector<int> v5(10);
+    // insertion_sort(v5);
+    cout << "v5 = " << v5 << "\n";
 
-  // test 0's and 1's
-  vector<int> v6;
-  for(int i=0; i<10; i++) {
-    if(i%2 == 0) v6.push_back(1);
-    else v6.push_back(0);
-  }
-  insertion_sort(v6, is_more);
-  cout << "v6 = " << v6 << "\n";
+    // test 0's and 1's
+    vector<int> v6;
+    for(int i=0; i<10; i++) {
+	if(i%2 == 0) v6.push_back(1);
+	else v6.push_back(0);
+    }
+    insertion_sort(v6, is_more);
+    cout << "v6 = " << v6 << "\n";
+}
+
+// test heap sort
+void test3()
+{
+    vector<int> v0{0, 22, 3, 2, 21};
+    heap_sort(v0);
+    cout << "v0 = " << v0 << "\n";
+  
+    // test large input
+    vector<int> v1;
+    for(int i=1000; i>0; i--) v1.push_back(i);
+    heap_sort(v1);
+    // cout << "v1 = " << v1 << "\n";
+
+    // test 0 as input
+    vector<int> v2;
+    heap_sort(v2);
+    cout << "v2 = " << v2 << "\n";
+
+    // test negative numbers
+    vector<int> v3;
+    for(int i=-1; i>-11; i--) v3.push_back(i);
+    heap_sort(v3);
+    cout << "v3 = " << v3 << "\n";
+
+    // test 1's as input
+    vector<int> v4{1, 1, 1, 1, 1};
+    heap_sort(v4);
+    cout << "v4 = " << v4 << "\n";
+
+    // test 0's as input
+    vector<int> v5(10);
+    // heap_sort(v5);
+    cout << "v5 = " << v5 << "\n";
+
+    // test 0's and 1's
+    vector<int> v6;
+    for(int i=0; i<10; i++) {
+	if(i%2 == 0) v6.push_back(1);
+	else v6.push_back(0);
+    }
+    heap_sort(v6, is_more);
+    cout << "v6 = " << v6 << "\n";
 }
 
 int main()
-  try {
-    test1();
-   return 0;
-  }
-  catch(exception& e) {
-    cerr << "Error: " << e.what();
-    return 1;
-  }
-  catch(...) {
-    cerr << "Oops! unknown exception";
-    return 2;
-  }
+    try {
+	test3();
+	return 0;
+    }
+    catch(exception& e) {
+	cerr << "Error: " << e.what();
+	return 1;
+    }
+    catch(...) {
+	cerr << "Oops! unknown exception";
+	return 2;
+    }
